5c7863ac38e789ef638ee042ef6fd1c1886b5750
mycloudrevolution <markus.kraus@gmail.com>
2018-10-31 19:56:11 +0100

    Basic - Get and Set Job Options

---
+++ Webinar-Job-Options.ps1
@@ -0,0 +1,26 @@
+﻿#region: Add Veeam PSSnapin and Connect
+Add-PSSnapin VeeamPSSnapin
+$VeeamCred = Get-Credential -Message "Veeam Credential"
+Connect-VBRServer -Server "192.168.3.100" -Credential $VeeamCred
+Find-VBRViEntity
+#endregion
+
+#region: My VeeamJobConfig Module
+Import-Module "C:\Users\Administrator\Documents\PowerShell\VeeamJobConfig\VeeamJobConfig.psd1"
+#endregion
+
+#region: Get Job Options
+$ExampleJob = Get-VBRJob -Name "Backup Job 1"
+$ExampleJobOptions = $ExampleJob.GetOptions()
+
+##Output Job Options as JSON
+$ExampleJobOptions | ConvertTo-Json
+#endregion
+
+#region: Basic - Set Job Options
+$BackupJob = Get-VBRJob -Name "Backup Job 2"
+Set-VBRJobOptions -Job $BackupJob -Options $ExampleJobOptions
+#endregion
+
+
+
