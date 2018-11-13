75f8f076f18ae83696795f9380be53c0a4206156
mycloudrevolution <markus.kraus@gmail.com>
2018-11-13 21:00:16 +0100

    Working with Module

--- Webinar-Job-Options.ps1
+++ Webinar-Job-Options.ps1
@@ -6,6 +6,7 @@ Find-VBRViEntity
 #endregion
 
 #region: My VeeamJobConfig Module
+Remove-Module VeeamJobConfig -ErrorAction SilentlyContinue
 Import-Module "C:\Users\Administrator\Documents\PowerShell\VeeamJobConfig\VeeamJobConfig.psd1"
 #endregion
 
@@ -14,7 +15,10 @@ $ExampleJob = Get-VBRJob -Name "Backup Job 1"
 $ExampleJobOptions = $ExampleJob.GetOptions()
 
 ##Output Job Options as JSON
-$ExampleJobOptions | ConvertTo-Json
+$JsonObject = $ExampleJobOptions | ConvertTo-Json
+$Object = $JsonObject | ConvertFrom-Json
+$Object.PSObject.Properties.Remove('Options')
+$Object | ConvertTo-Json | Out-File "C:\temp\ExampleJobOptions.json"
 #endregion
 
 #region: Basic - Set Job Options
@@ -27,4 +31,24 @@ Set-VBRJobOptions -Job $BackupJob -Options $ExampleJobOptions
 $BackupJobOptions = $BackupJob.GetOptions()
 $BackupJobOptions.BackupStorageOptions.EnableDeletedVmDataRetention = $True
 Set-VBRJobOptions -Job $BackupJob -Options $BackupJobOptions
+#endregion
+
+#region: Working with Module
+## Export a Config
+$TemplateBackupJob = Get-VBRJob -Name "Template Job 1"
+Export-VbrJobOptionsToFile -BackupJob $TemplateBackupJob -Path "C:\temp\ExampleJobOptions.json"
+
+## Update Multiple Jobs from File
+Get-VBRJob -Name "Backup Job*" | ft -AutoSize
+(Get-VBRJob -Name "Backup Job*" | Get-VBRJobOptions).BackupStorageOptions | ft -AutoSize
+
+Get-VBRJob -Name "Backup Job*" | Set-VBRJobOptionsFromFile -ReferenceFile "C:\temp\ExampleJobOptions.json" -BackupStorageOptions
+
+(Get-VBRJob -Name "Backup Job*" | Get-VBRJobOptions).BackupStorageOptions | ft -AutoSize
+
+## Update Multiple Jobs from Template
+$TemplateBackupJob = Get-VBRJob -Name "Template Job 2"
+Get-VBRJob -Name "Backup Job*" | Set-VBRJobOptionsFromRef -ReferenceBackupJob $TemplateBackupJob 
+
+(Get-VBRJob -Name "Backup Job*" | Get-VBRJobOptions).BackupStorageOptions | ft -AutoSize
 #endregion
